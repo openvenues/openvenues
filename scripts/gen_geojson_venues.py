1ad46d85d08586a4bab8ae88569788c3ca743ceb
Al <albarrentine@gmail.com>
2015-07-02 17:39:01 -0400

    [geojson] building all files at once, using the same GUIDs

--- scripts/gen_geojson_venues.py
+++ scripts/gen_geojson_venues.py
@@ -16,10 +16,12 @@ from openvenues.extract.util import *
 
 logger = logging.getLogger('geojson')
 
+
 def random_guid():
     return str(uuid.uuid4())
 
-def gen_venues(d, require_latlon=True):
+
+def gen_venues(d):
     for filename in os.listdir(d):
         f = open(os.path.join(d, filename))
         for line in f:
@@ -43,14 +45,14 @@ def gen_venues(d, require_latlon=True):
                 props = None
 
                 if item_type in (SCHEMA_DOT_ORG_TYPE, RDFA_TYPE):
-                    props = schema_dot_org_props(item, item_type, require_latlon=require_latlon)
+                    props = schema_dot_org_props(item, item_type, require_latlon=False)
                 elif item_type in (OG_TAG_TYPE, OG_BUSINESS_TAG_TYPE):
                     og_type = item.get('og:type', '').rsplit(':', 1)[-1].strip().lower()
                     if og_type not in OG_PLACE_TYPES and not (og_type == 'article' and OG_WHITELIST_DOMAINS.search(domain)):
                         continue
-                    props = og_props(item, item_type, require_latlon=require_latlon)
+                    props = og_props(item, item_type, require_latlon=False)
                 elif item_type == VCARD_TYPE:
-                    props = vcard_props(item, require_latlon=require_latlon)
+                    props = vcard_props(item, require_latlon=False)
 
                 if props:
                     venues.append(props)
@@ -63,7 +65,7 @@ def midpoint(x1, x2):
     return float(x1 + x2) / 2
 
 
-def main(input_dir, output_dir, require_latlon=True):
+def main(input_dir, output_dir):
     formatter = logging.Formatter('%(asctime)s %(levelname)s [%(name)s]: %(message)s')
     handler = logging.StreamHandler(sys.stderr)
     handler.setFormatter(formatter)
@@ -88,17 +90,15 @@ def main(input_dir, output_dir, require_latlon=True):
             city_names.append(city)
             i += 1
 
-    if require_latlon:
-        files = {name: open(os.path.join(output_dir, 'cities', '{}.geojson'.format(name)), 'w') for name in city_names}
+    files = {name: open(os.path.join(output_dir, 'cities', '{}.geojson'.format(name)), 'w') for name in city_names}
 
-    planet_name = 'planet.geojson' if require_latlon else 'planet_addresses_only.json'
-
-    planet = open(os.path.join(output_dir, planet_name), 'w')
+    planet = open(os.path.join(output_dir, 'planet.geojson'), 'w')
+    planet_addresses_only = open(os.path.join(output_dir, 'planet_addresses_only.json'), 'w')
 
     i = 0
     seen = set()
 
-    for url, canonical, venues in gen_venues(input_dir, require_latlon=require_latlon):
+    for url, canonical, venues in gen_venues(input_dir):
         domain = urlparse.urlsplit(url).netloc.strip('www.')
         for props in venues:
             lat = props.get('latitude')
@@ -107,50 +107,47 @@ def main(input_dir, output_dir, require_latlon=True):
             props['url'] = url
             street = props.get('street_address')
             name = props.get('name')
-            if require_latlon:
-                h = hash((name, street, lat, lon, domain))
-            else:
-                h = hash((name, street, domain))
+            planet_hash = hash((name, street, lat, lon, domain))
+            address_hash = hash((name, street, domain))
             props['guid'] = props.get('guid', random_guid())
             venue = venue_to_geojson(props)
-            if require_latlon and lat is not None and lon is not None and h not in seen:
+            if lat is not None and lon is not None and planet_hash not in seen:
                 cities = list(rtree.intersection((lon, lat, lon, lat)))
                 if cities:
                     for c in cities:
                         f = files[city_names[c]]
                     f.write(json.dumps(venue) + '\n')
-            if h not in seen:
-                if not require_latlon:
-                    venue = props
+            if planet_hash not in seen:
                 planet.write(json.dumps(venue) + '\n')
-
-            seen.add(h)
+                seen.add(planet_hash)
+            if address_hash not in seen:
+                planet_addresses_only.write(json.dumps(props) + '\n')
+                seen.add(address_hash)
             i += 1
             if i % 1000 == 0 and i > 0:
                 logger.info('did {}'.format(i))
 
-    if require_latlon:
-        logger.info('Creating manifest files')
+    logger.info('Creating manifest files')
 
-        manifest_files = []
+    manifest_files = []
 
-        for k, v in all_cities['regions'].iteritems():
-            for city, data in v['cities'].iteritems():
-                f = files[city]
-                if f.tell() == 0:
-                    f.close()
-                    os.unlink(os.path.join(output_dir, 'cities', '{}.geojson'.format(city)))
-                    continue
+    for k, v in all_cities['regions'].iteritems():
+        for city, data in v['cities'].iteritems():
+            f = files[city]
+            if f.tell() == 0:
+                f.close()
+                os.unlink(os.path.join(output_dir, 'cities', '{}.geojson'.format(city)))
+                continue
 
-                bbox = data['bbox']
-                lat = midpoint(float(bbox['top']), float(bbox['bottom']))
-                lon = midpoint(float(bbox['left']), float(bbox['right']))
+            bbox = data['bbox']
+            lat = midpoint(float(bbox['top']), float(bbox['bottom']))
+            lon = midpoint(float(bbox['left']), float(bbox['right']))
 
-                manifest_files.append({'latitude': lat, 'longitude': lon, 'file': '{}.geojson'.format(city), 'name': city.replace('_', ', ').replace('-', ' ').title()})
+            manifest_files.append({'latitude': lat, 'longitude': lon, 'file': '{}.geojson'.format(city), 'name': city.replace('_', ', ').replace('-', ' ').title()})
 
-        manifest = {'files': manifest_files}
+    manifest = {'files': manifest_files}
 
-        json.dump(manifest, open(os.path.join(output_dir, 'manifest.json'), 'w'))
+    json.dump(manifest, open(os.path.join(output_dir, 'manifest.json'), 'w'))
 
     logger.info('Done!')
 
@@ -158,7 +155,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_dir')
     parser.add_argument('output_dir')
-    parser.add_argument('--no-lat-lon', dest='require_latlon', action='store_false', default=True)
     args = parser.parse_args()
 
-    main(args.input_dir, args.output_dir, require_latlon=args.require_latlon)
+    main(args.input_dir, args.output_dir)
